b2753df426e01172220f8d5f08681f64052c3ea5
GlitchX <jayanthadhithyaagr@gmail.com>
2025-10-05 18:14:02 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// TensorFlow.js backend configuration
+import * as tf from '@tensorflow/tfjs'
+
+// Set TensorFlow backend
+tf.setBackend('cpu').then(() => {
+  console.log('TensorFlow.js backend initialized:', tf.getBackend())
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
